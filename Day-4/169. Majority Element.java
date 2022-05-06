aea673682174832f9fd0fa95ab96a7db00da605e
Nikhitha-1612 <96165205+Nikhitha-1612@users.noreply.github.com>
2022-05-06 11:06:36 +0530

    Create 169. Majority Element.java

---
+++ Day-4/169. Majority Element.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+}
